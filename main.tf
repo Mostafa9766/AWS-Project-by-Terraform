6053edc71e8c2187ea255725e0e6b3b0422b7336
Mostafa Ahmed <mostafa.97ahmed.ma@gmail.com>
2022-09-24 14:54:52 +0200

    first commit

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "network" {
-    source="/home/mostafa/Documents/network"
-    vpc_cidr=var.vpc_cidr
-    cidr_public1=var.cidr_public1
-    cidr_public2=var.cidr_public2
-    cidr_private1=var.cidr_private1
-    cidr_private2=var.cidr_private2
-    region=var.region
+  source        = "/home/mostafa/Documents/network"
+  vpc_cidr      = var.vpc_cidr
+  cidr_public1  = var.cidr_public1
+  cidr_public2  = var.cidr_public2
+  cidr_private1 = var.cidr_private1
+  cidr_private2 = var.cidr_private2
+  region        = var.region
 }
